9230917264d97f13b3ac94456401f1b091a2f5e3
ILIA <dubenskiiilia@gmail.com>
2023-05-25 12:32:11 +0700

    Initial Commit

---
+++ GameOfLife/GameOfLifeApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameOfLifeApp.swift
+//  GameOfLife
+//
+//  Created by Илья Дубенский on 25.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct GameOfLifeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
